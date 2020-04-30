e925b2504c791fb796a250673c84081492bd7f33
Kyle Follendorf <ksfollendorf@gmail.com>
2020-04-29 18:14:00 -0700

    added 2d array for gameboard

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,3 +1,4 @@
+=begin
 puts "Player One, type your name and press 'Enter'"
 player_one = gets.chomp
 puts "Player Two, type your name and press 'Enter'"
@@ -15,3 +16,21 @@ end
 if flip == "flip"
     puts "#{[player_one, player_two].sample} goes first!"
 end
+
+# Display Gameboard here now.
+# Player X Choose where they would like to go by typing in the value i.e [3,2]
+# Display gameboard with Player X choice on it
+# Prompt next player to choose 
+# Repeat
+
+=end
+
+
+array = Array.new(3) { Array.new(3){ "x" } }
+array_string = <<~MYHEREDOC
+  #{array[0]}
+  #{array[1]}
+  #{array[2]}
+MYHEREDOC
+
+puts array_string
