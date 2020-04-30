10cba66b570d0b7fbc896a888df7572a445f212d
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-04-30 16:24:09 -0700

    Added info for classes

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,4 +1,28 @@
+
+
+
+
+
+
+
+
+
+
 =begin
+*Starting over with implementing the below into classes.
+here's an example:
+_____________________________________________
+
+1)Tictactoe the main driver class in charge of initializing a new players etc,
+ displaying the game rules, and looping till the games done etc
+2)Board in charge maintaining the state of the board and,
+ displaying it, checking if spots are free etc
+3)player which maintains the player state ("x" or "o")
+ and can be asked to take a turn etc to get their move
+4)computer player which inherits from player above overriding the ability to ask
+ for a turn with logic to programatically pick a spot to place a piece etc
+____________________________________________
+
 puts "Player One, type your name and press 'Enter'"
 player_one = gets.chomp
 puts "Player Two, type your name and press 'Enter'"
@@ -13,24 +37,110 @@ while flip != "flip"
     flip = gets.chomp.downcase
 end
 
+puts
+
 if flip == "flip"
-    puts "#{[player_one, player_two].sample} goes first!"
+    starting_player = [player_one, player_two].sample
+    puts "#{starting_player} goes first and will use 'X'!"
 end
 
-# Display Gameboard here now.
-# Player X Choose where they would like to go by typing in the value i.e [3,2]
-# Display gameboard with Player X choice on it
-# Prompt next player to choose 
-# Repeat
+puts
+
+def display_board(array)
+  <<~MYHEREDOC
+    #{array[0]}
+    #{array[1]}
+    #{array[2]}
+  MYHEREDOC
+  end
+  my_array = Array.new(3) { Array.new(3){ " " } }
+  puts display_board(my_array)
+
+puts
+
+puts "It's #{starting_player}'s turn. Pick your move by identifying which row followed by which column.
+Example: 1,2 will place an 'X' in the first row, second column."
+
+move = gets.chomp
+if move == "1,1"
+  my_array[0][0] = "X"
+elsif move == "1,2"
+  my_array[0][1] = "X"
+  elsif move == "1,3"
+    my_array[0][2] = "X"
+  elsif move == "2,1"
+    my_array[1][0] = "X"
+  elsif move == "2,2"
+    my_array[1][1] = "X"
+  elsif move == "2,3"
+    my_array[1][2] = "X"
+  elsif move == "3,1"
+    my_array[2][0] = "X"
+  elsif move == "3,2"
+    my_array[2][1] = "X"
+  elsif move == "3,3"
+    my_array[2][2] = "X"
+end
+puts
+
+puts display_board(my_array)
+puts
+
+if player_one != starting_player
+  puts "It's #{player_one}'s turn. Remember, you're 'O'!"
+elsif player_two != starting_player
+  puts "It's #{player_two}'s turn. Remember, you're 'O'!"
+end
+
+
+move = gets.chomp
+if move == "1,1"
+  my_array[0][0] = "O"
+elsif move == "1,2"
+  my_array[0][1] = "O"
+  elsif move == "1,3"
+    my_array[0][2] = "O"
+  elsif move == "2,1"
+    my_array[1][0] = "O"
+  elsif move == "2,2"
+    my_array[1][1] = "O"
+  elsif move == "2,3"
+    my_array[1][2] = "O"
+  elsif move == "3,1"
+    my_array[2][0] = "O"
+  elsif move == "3,2"
+    my_array[2][1] = "O"
+  elsif move == "3,3"
+    my_array[2][2] = "O"
+end
+puts
+
+puts display_board(my_array)
+puts
+
+move = gets.chomp
+if move == "1,1"
+  my_array[0][0] = "X"
+elsif move == "1,2"
+  my_array[0][1] = "X"
+  elsif move == "1,3"
+    my_array[0][2] = "X"
+  elsif move == "2,1"
+    my_array[1][0] = "X"
+  elsif move == "2,2"
+    my_array[1][1] = "X"
+  elsif move == "2,3"
+    my_array[1][2] = "X"
+  elsif move == "3,1"
+    my_array[2][0] = "X"
+  elsif move == "3,2"
+    my_array[2][1] = "X"
+  elsif move == "3,3"
+    my_array[2][2] = "X"
+end
+puts
+
+puts display_board(my_array)
+puts
 
 =end
-
-
-array = Array.new(3) { Array.new(3){ "x" } }
-array_string = <<~MYHEREDOC
-  #{array[0]}
-  #{array[1]}
-  #{array[2]}
-MYHEREDOC
-
-puts array_string
