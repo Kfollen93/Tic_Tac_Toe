c90a76ee0960e5fbb392a9a72bc4545cb3c7467a
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-02 17:42:18 -0700

    Removed 'rules' method, printed directly in 'initialize'

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,15 +1,47 @@
 class TicTacToe
   def initialize
-    @board = Board.new
-    @board.create_board
-  end
+    @board = Board.new #initalizes board
+    #print rules
+    puts "Player One, type your name and press 'Enter'"
+    player_one = gets.chomp
+    puts "Player Two, type your name and press 'Enter'"
+    player_two = gets.chomp
+    puts 'Welcome to Tic-Tac-Toe. Three of the same letter (X or O)
+    in a row, column, or diagonal, wins the game.'
 
-  def rules
-    puts "Here's how we play tictactoe etc"
+    puts "Type 'Flip' to flip a coin to see who goes first"
+    flip = gets.chomp.downcase
+    while flip != "flip"
+    puts "Yo! Type 'Flip' to decide who starts."
+    flip = gets.chomp.downcase
+    end
+
+puts
+
+if flip == "flip"
+    starting_player = [player_one, player_two].sample
+    puts "#{starting_player} goes first and will use 'X'!"
+end
+
+puts
+    @board.create_board #displays board
+    puts "It's #{starting_player}'s turn. Pick your move by identifying which row followed by which column.
+    Example: 1,2 will place an 'X' in the first row, second column."
+    #have starting_player pick an array spot '1,1' i.e.
+    #check it == " " and then place X.
+    #display updated board
+    #prompt second_player pick an array spot '2,2' i.e.
+    #check it == " " and then place O.
+    #display updated board
+    #REPEAT LOOP
+    #once the array values horizontal, vertical, diagnol 3 in a row end loop
+    #announce winner
+    #if no more spaces == " " announce tie.
   end
 
   def loop_until_finished
   end  
+
 end
 
 class Board
@@ -31,6 +63,7 @@ class Player
 end
 
 TicTacToe.new
+
 
 
 
