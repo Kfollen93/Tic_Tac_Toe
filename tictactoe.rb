2ee5af6d8903a5efe1680ffd9db56879e56a2a55
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-05 16:03:16 -0700

    Finished game logic

--- tictactoe.rb
+++ tictactoe.rb
@@ -2,17 +2,44 @@ class TicTacToe
   def initialize
     @board = Board.new # initalizes board
     # print rules
-    puts "Player One, type your name and press 'Enter'"
+    puts "               [/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/]"
+    puts "               [/]                                                                  [/]"
+    puts "               [/]           Player One, type your name and press 'Enter'           [/]"
+    puts "               [/]                                                                  [/]"
+    puts "               [/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/]"
     $player_one = gets.chomp
-    puts "Player Two, type your name and press 'Enter'"
+    puts "               [/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/]"
+    puts "               [/]                                                                  [/]"
+    puts "               [/]           Player Two, type your name and press 'Enter'           [/]"
+    puts "               [/]                                                                  [/]"
+    puts "               [/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/]"
     $player_two = gets.chomp
-    puts 'Welcome to Tic-Tac-Toe. Three of the same letter (X or O)
-    in a row, column, or diagonal, wins the game.'
-
-    puts "Type 'Flip' to flip a coin to see who goes first"
+    puts "               [/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/]"
+    puts "               |/|                           Tic-Tac-Toe                            |/|"
+    puts "               |/|                                                                  |/|"
+    puts '               |/|      Welcome to Tic-Tac-Toe. Three of the same letter (X or O)   |/|'
+    puts '               |/|            in a row, column, or diagonal, wins the game.         |/|'
+    puts '               |/|          If no spaces are left, it will be declared a tie.       |/|'
+    puts '               |/|                                                                  |/|'
+    puts '               |/|      You move by selecting a row (0-2), and pressing "Enter",    |/|'
+    puts '               |/|     followed by selecting a column (0-2), and pressing "Enter".  |/|'
+    puts '               |/|                                                                  |/|'
+    puts '               |/|                                                                  |/|'
+    puts "               [/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/]"
+    puts
+    puts
+    puts "               [/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/]"
+    puts "               [/]                                                                  [/]"
+    puts "               [/]           Type 'Flip' to decide who will start first.            [/]"
+    puts "               [/]                                                                  [/]"
+    puts "               [/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/]"
     flip = gets.chomp.downcase
     while flip != "flip"
-      puts "Yo! Type 'Flip' to decide who starts."
+      puts "             [/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/]"
+      puts "             [/]                                                                  [/]"
+      puts "             [/]   Yo! You need to type 'Flip' to decide who will start first!    [/]"
+      puts "             [/]                                                                  [/]"
+      puts "             [/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/]"
       flip = gets.chomp.downcase
     end
 
@@ -20,12 +47,8 @@ class TicTacToe
 
     if flip == "flip"
       $starting_player = [$player_one, $player_two].sample
-      puts "#{$starting_player} goes first and will use 'X'!"
+      puts "                       [/][/]     #{$starting_player} goes first and will use 'X'!       [/][/]"
     end
-
-    puts
-    puts "It's #{$starting_player}'s turn. Pick your move by identifying which row followed by which column.
-    Example: 1,2 will place an 'X' in the first row, second column."
     @board.create_board # displays board
   end
 
@@ -43,51 +66,125 @@ class Board
   end
 
   def create_board
-    
     my_array = Array.new(3) { Array.new(3) { " " } }
     puts display_board(my_array)
-    row = gets.chomp.to_i
-    column = gets.chomp.to_i
-    while row < 0 || row > 2 || column < 0 || column > 2
-      puts "Please enter between a range of 0 - 2"
+    loop do
       row = gets.chomp.to_i
       column = gets.chomp.to_i
-    end
-    while my_array[row][column] != " "
-      puts "Sorry you can't move there. Try again."
+      while row < 0 || row > 2 || column < 0 || column > 2
+        puts "Please enter between a range of 0 - 2"
+        row = gets.chomp.to_i
+        column = gets.chomp.to_i
+      end
+      while my_array[row][column] != " "
+        puts "Sorry you can't move there. Try again."
+        row = gets.chomp.to_i
+        column = gets.chomp.to_i
+      end
+
+      if my_array[row][column] == " "
+        my_array[row][column] = "X"
+      end
+      puts
+      puts display_board(my_array)
+      break if
+      # horizontal lines
+      my_array[0][0] == "X" && my_array[0][1] == "X" && my_array[0][2] == "X" ||
+      my_array[1][0] == "X" && my_array[1][1] == "X" && my_array[1][2] == "X" ||
+      my_array[2][0] == "X" && my_array[2][1] == "X" && my_array[2][2] == "X" ||
+      my_array[0][0] == "O" && my_array[0][1] == "O" && my_array[0][2] == "O" ||
+      my_array[1][0] == "O" && my_array[1][1] == "O" && my_array[1][2] == "O" ||
+      my_array[2][0] == "O" && my_array[2][1] == "O" && my_array[2][2] == "O" ||
+
+      # vertical lines
+      my_array[0][0] == "X" && my_array[1][0] == "X" && my_array[2][0] == "X" ||
+      my_array[0][1] == "X" && my_array[1][1] == "X" && my_array[2][1] == "X" ||
+      my_array[0][2] == "X" && my_array[1][2] == "X" && my_array[2][2] == "X" ||
+      my_array[0][0] == "O" && my_array[1][0] == "O" && my_array[2][0] == "O" ||
+      my_array[0][1] == "O" && my_array[1][1] == "O" && my_array[2][1] == "O" ||
+      my_array[0][2] == "O" && my_array[1][2] == "O" && my_array[2][2] == "O" ||
+
+      # diagonals
+      my_array[0][0] == "X" && my_array[1][1] == "X" && my_array[2][2] == "X" ||
+      my_array[2][0] == "X" && my_array[1][1] == "X" && my_array[0][2] == "X" ||
+      my_array[0][0] == "O" && my_array[1][1] == "O" && my_array[2][2] == "O" ||
+      my_array[2][0] == "O" && my_array[1][1] == "O" && my_array[0][2] == "O"
+
+      puts
+      if $player_one != $starting_player
+        puts "    [^][^]     It's #{$player_one}'s turn!       [^][^]"
+      else puts "   [^][^]     It's #{$player_two}'s turn!       [^][^]"
+      end
+      puts
+
       row = gets.chomp.to_i
       column = gets.chomp.to_i
-    end
+      while row < 0 || row > 2 || column < 0 || column > 2
+        puts "Please enter between a range of 0 - 2"
+        row = gets.chomp.to_i
+        column = gets.chomp.to_i
+      end
+      while my_array[row][column] != " "
+        puts "Sorry you can't move there. Try again."
+        row = gets.chomp.to_i
+        column = gets.chomp.to_i
+      end
 
-    if my_array[row][column] == " "
-      my_array[row][column] = "X"
-    end
-    puts display_board(my_array)
+      if my_array[row][column] == " "
+        my_array[row][column] = "O"
+      end
+      puts display_board(my_array)
+      break if
+      # horizontal lines
+      my_array[0][0] == "X" && my_array[0][1] == "X" && my_array[0][2] == "X" ||
+      my_array[1][0] == "X" && my_array[1][1] == "X" && my_array[1][2] == "X" ||
+      my_array[2][0] == "X" && my_array[2][1] == "X" && my_array[2][2] == "X" ||
+      my_array[0][0] == "O" && my_array[0][1] == "O" && my_array[0][2] == "O" ||
+      my_array[1][0] == "O" && my_array[1][1] == "O" && my_array[1][2] == "O" ||
+      my_array[2][0] == "O" && my_array[2][1] == "O" && my_array[2][2] == "O" ||
 
-    if $player_one != $starting_player
-      puts "It's #{$player_one}'s turn"
-    else puts "It's #{$player_two}'s turn"
-    end
+      # vertical lines
+      my_array[0][0] == "X" && my_array[1][0] == "X" && my_array[2][0] == "X" ||
+      my_array[0][1] == "X" && my_array[1][1] == "X" && my_array[2][1] == "X" ||
+      my_array[0][2] == "X" && my_array[1][2] == "X" && my_array[2][2] == "X" ||
+      my_array[0][0] == "O" && my_array[1][0] == "O" && my_array[2][0] == "O" ||
+      my_array[0][1] == "O" && my_array[1][1] == "O" && my_array[2][1] == "O" ||
+      my_array[0][2] == "O" && my_array[1][2] == "O" && my_array[2][2] == "O" ||
 
-    row = gets.chomp.to_i
-    column = gets.chomp.to_i
-    while row < 0 || row > 2 || column < 0 || column > 2
-      puts "Please enter between a range of 0 - 2"
-      row = gets.chomp.to_i
-      column = gets.chomp.to_i
-    end
-    while my_array[row][column] != " "
-      puts "Sorry you can't move there. Try again."
-      row = gets.chomp.to_i
-      column = gets.chomp.to_i
-    end
+      # diagonals
+      my_array[0][0] == "X" && my_array[1][1] == "X" && my_array[2][2] == "X" ||
+      my_array[2][0] == "X" && my_array[1][1] == "X" && my_array[0][2] == "X" ||
+      my_array[0][0] == "O" && my_array[1][1] == "O" && my_array[2][2] == "O" ||
+      my_array[2][0] == "O" && my_array[1][1] == "O" && my_array[0][2] == "O"
 
-    if my_array[row][column] == " "
-      my_array[row][column] = "O"
-    end
-    puts display_board(my_array)
+      puts
+      puts "    [^][^]     It's #{$starting_player}'s turn!       [^][^]"
+    end # game loop
+    if
+      # horizontal lines
+      my_array[0][0] == "X" && my_array[0][1] == "X" && my_array[0][2] == "X" ||
+      my_array[1][0] == "X" && my_array[1][1] == "X" && my_array[1][2] == "X" ||
+      my_array[2][0] == "X" && my_array[2][1] == "X" && my_array[2][2] == "X" ||
+      my_array[0][0] == "O" && my_array[0][1] == "O" && my_array[0][2] == "O" ||
+      my_array[1][0] == "O" && my_array[1][1] == "O" && my_array[1][2] == "O" ||
+      my_array[2][0] == "O" && my_array[2][1] == "O" && my_array[2][2] == "O" ||
 
-    puts "It's #{$starting_player}'s turn"
+      # vertical lines
+      my_array[0][0] == "X" && my_array[1][0] == "X" && my_array[2][0] == "X" ||
+      my_array[0][1] == "X" && my_array[1][1] == "X" && my_array[2][1] == "X" ||
+      my_array[0][2] == "X" && my_array[1][2] == "X" && my_array[2][2] == "X" ||
+      my_array[0][0] == "O" && my_array[1][0] == "O" && my_array[2][0] == "O" ||
+      my_array[0][1] == "O" && my_array[1][1] == "O" && my_array[2][1] == "O" ||
+      my_array[0][2] == "O" && my_array[1][2] == "O" && my_array[2][2] == "O" ||
+
+      # diagonals
+      my_array[0][0] == "X" && my_array[1][1] == "X" && my_array[2][2] == "X" ||
+      my_array[2][0] == "X" && my_array[1][1] == "X" && my_array[0][2] == "X" ||
+      my_array[0][0] == "O" && my_array[1][1] == "O" && my_array[2][2] == "O" ||
+      my_array[2][0] == "O" && my_array[1][1] == "O" && my_array[0][2] == "O"
+      
+      #puts "You win!"
+    end
 end # create board end
 end # class end
 
