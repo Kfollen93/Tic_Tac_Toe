a106c5ec5e789a8ec732b32e73aa316bc35f4176
Kyle Follendorf <ksfollendorf@gmail.com>
2020-07-27 10:30:11 -0700

    Initial commit, part of refactor

---
+++ instructions.rb
@@ -0,0 +1,53 @@
+require_relative 'player'
+module Instructions
+    def display_instructions
+        puts "               [/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/]"
+    puts "               [/]                                                                  [/]"
+    puts "               [/]           Player One, type your name and press 'Enter'           [/]"
+    puts "               [/]                                                                  [/]"
+    puts "               [/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/]"
+    @player_one = gets.chomp
+    puts "               [/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/]"
+    puts "               [/]                                                                  [/]"
+    puts "               [/]           Player Two, type your name and press 'Enter'           [/]"
+    puts "               [/]                                                                  [/]"
+    puts "               [/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/]"
+    @player_two = gets.chomp
+    puts "               [/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/]"
+    puts "               |/|                           Tic-Tac-Toe                            |/|"
+    puts "               |/|                                                                  |/|"
+    puts '               |/|      Welcome to Tic-Tac-Toe. Three of the same letter (X or O)   |/|'
+    puts '               |/|            in a row, column, or diagonal, wins the game.         |/|'
+    puts '               |/|          If no spaces are left, it will be declared a tie.       |/|'
+    puts '               |/|                                                                  |/|'
+    puts '               |/|      You move by selecting a row (0-2), and pressing "Enter",    |/|'
+    puts '               |/|     followed by selecting a column (0-2), and pressing "Enter".  |/|'
+    puts '               |/|                                                                  |/|'
+    puts '               |/|                                                                  |/|'
+    puts "               [/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/]"
+    puts
+    puts
+    puts "               [/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/]"
+    puts "               [/]                                                                  [/]"
+    puts "               [/]           Type 'Flip' to decide who will start first.            [/]"
+    puts "               [/]                                                                  [/]"
+    puts "               [/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/]"
+    flip = gets.chomp.downcase
+    while flip != "flip"
+      puts "             [/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/]"
+      puts "             [/]                                                                  [/]"
+      puts "             [/]   Yo! You need to type 'Flip' to decide who will start first!    [/]"
+      puts "             [/]                                                                  [/]"
+      puts "             [/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/][/]"
+      flip = gets.chomp.downcase
+    end
+
+    randomize_player
+
+  end
+
+  def randomize_player
+      @starting_player = [@player_one, @player_two].sample
+      puts "                       [/][/]     #{@starting_player} goes first and will use 'X'!       [/][/]"
+  end
+end
